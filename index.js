d65345cb02905acd5a433cb13d45496e02a6c9d9
SYlence_L <markyan2307@gmail.com>
2026-05-15 15:10:53 +0300

    configured js logic partialy

--- index.js
+++ index.js
@@ -1,3 +1,79 @@
 const gameBoard = document.getElementById("gameBoard");
 const ctx = gameBoard.getContext("2d");
-const scoreText = document.querySelector("#scoreText")   
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("resetBtn");
+const gameWidth = gameBoard.width;
+const gameHeitght = gameBoard.height;
+const boardBackground = "white";
+const snakeColor = "lightgreen";
+const snakeBorder = "black";
+const foodColor = "red";
+const unitSize = 25;
+let running = false;
+let xVelocity = unitSize;
+let yVelocity = 0;
+let foodX;
+let foodY;
+let score = 0;
+let snake = [
+    { x: unitSize * 4, y: 0 },
+    { x: unitSize * 3, y: 0 },
+    { x: unitSize * 2, y: 0 },
+    { x: unitSize, y: 0 },
+    { x: 0, y: 0 }
+];
+
+window.addEventListener("keydown", changeDirection);
+
+gameStart();
+
+function gameStart() {
+    running = true;
+    scoreText.textContent = score;
+    createFood();
+    drawFood();
+    nextTick();
+};
+function nextTick() {
+    if (running) {
+        setTimeout(() => {
+            clearBoard();
+            drawFood();
+            moveSnake();
+            drawSnake();
+            checkGameOver();
+            nextTick();
+        }, 75);
+    }
+};
+function clearBoard() {
+    ctx.fillStyle = boardBackground;
+    ctx.fillRect(0, 0, gameWidth, gameHeitght);
+};
+function createFood() {
+    const randomFood = (min, max) => {
+        const randNum = Math.round((Math.random() * (max - min) + min) / unitSize) * unitSize;
+        return randNum;
+    };
+    foodX = randomFood(0, gameWidth - unitSize);
+    foodY = randomFood(0, gameWidth - unitSize);
+
+};
+
+
+function drawFood() {
+    ctx.fillStyle = foodColor;
+    ctx.fillRect(foodX, foodY, unitSize, unitSize);
+};
+
+function moveSnake() { };
+function drawSnake() {
+
+};
+function changeDirection() { };
+function checkGameOver() { };
+function displayGameOver() { };
+function resetGame() { };
+
+
+
