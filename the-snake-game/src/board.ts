d35455d944798658f4b951efe99325d400a5c734
SYlence_L <markyan2307@gmail.com>
2026-05-18 16:29:05 +0300

    removed legacy code. Built the project to deploy. Deployed

--- the-snake-game/src/board.ts
+++ the-snake-game/src/board.ts
@@ -1,13 +1,13 @@
 import { boardBackground, gameWidth, gameHeitght } from "./constants";
 
 export const gameBoard = document.getElementById("gameBoard") as HTMLCanvasElement;
-export const ctx = gameBoard.getContext("2d");
-export const scoreText = document.querySelector("#scoreText");
-export const resetBtn = document.querySelector("#resetBtn");
+export const ctx = gameBoard.getContext("2d")!;
+export const scoreText = document.querySelector("#scoreText") as HTMLElement;
+export const resetBtn = document.querySelector("#resetBtn") as HTMLButtonElement;
 
-export const dialog = document.getElementById("customDialog");
-export const btnYes = document.getElementById("btnYes");
-export const btnNo = document.getElementById("btnNo");
+export const dialog = document.getElementById("customDialog") as HTMLElement;
+export const btnYes = document.getElementById("btnYes") as HTMLButtonElement;
+export const btnNo = document.getElementById("btnNo") as HTMLButtonElement;
 
 
 export function clearBoard() {
